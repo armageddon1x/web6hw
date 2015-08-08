7171524ad7573cb9f49a77d3dce6f7e13a69e409
armageddon <armageddon1x@gmail.com>
2015-08-08 17:22:27 -0400

    added some jquery notes

--- js/function.js
+++ js/function.js
@@ -1,3 +1,47 @@
 //*************************************************
 //jQuery
 //*************************************************
+
+//Javascript and jQuery
+
+/*Javascript is powerful but can be overwhelming. 
+however, there are libraries of functions premade so 
+that it is easier to implement JS into web pages. one
+library is jQuery, which can be included into the 
+html head as an external source. this library is held
+in a CNS (content deliery network) with an absolute 
+path*/
+
+//how does a jQuery function look like?
+
+//call
+//$()
+/*this is the way to call jQuery functions. the $() is 
+actually the main jQuery function that contains the 
+library. by using the $(), you can call other functions
+as well*/
+
+//add document
+//$(document)
+/*elements to be used by the function go inside the
+parentheses. the elements are in CSS syntax*/
+
+//$("body");
+//$("#icon");
+//$(".thing"); 
+
+/*Javascript is an object oriented language, so it 
+manipulates objects. the Document Object Model 
+(DOM) is a standard that helps with interaction with 
+objects and using HTML elements as objects. jQuery 
+uses this to select basically any HTML element and 
+manipuate it easily.*/
+
+//check if document is ready
+//$(document).ready()
+
+//add anonymous function
+//$(document).ready(function(){
+			//thing to be done
+			//});
+
